aea72211b67246746454f9fd972c88d6d3bb48e7
Joe Hawes <morehawes@gmail.com>
2023-06-16 15:27:15 -0700

    Now using Laravel

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import QueriesView from './views/QueriesView.vue'
+</script>
+
+<template>
+  <QueriesView />
+</template>
+
+<style>
+body, html {
+	padding: 0;
+	margin: 0;
+}
+#app {
+	position: relative;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
