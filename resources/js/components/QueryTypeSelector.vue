63eb7160470281e9ccbd4311545fc3693f4aa786
Joe Hawes <>
2023-06-26 13:50:11 -0700

    Basic shopValues dropdown

--- resources/js/components/QueryTypeSelector.vue
+++ resources/js/components/QueryTypeSelector.vue
@@ -1,5 +1,7 @@
 <script setup>
+import { onMounted } from 'vue';
 import { useQueryStore } from '@/stores/queries';
+import shopValues from '../../json/shopValues.json';
 
 const queryStore = useQueryStore();
 
@@ -11,8 +13,28 @@ defineProps({
 <template>
 	<select
 		:value="modelValue"
-		@input="$emit('update:modelValue', $event.target.value)"		
+		@input="$emit('update:modelValue', $event.target.value)"
 	>
-		<option v-for="(value, key) in queryStore.getTypes" :value="key">{{ value }}</option>												
+		<option
+		    v-for="shop in shopValues"
+		    :value="shop.Value"
+		    :disabled="! shop.Value"
+		    :style="shop.Value ? 'background-image:url(resources/img/symbols/shop/' + shop.Value + '.svg);' : ''"
+		>{{ shop.Value ? shop.Value : shop.Key }}</option>
 	</select>
 </template>
+
+<style lang="less">
+    select {
+        width: 100%;
+        background: #fff;
+        border: 1px solid #333;
+        option {
+            background: #fff;
+            border: 1px solid #333;
+            &[disabled] {
+                color: red;
+            }
+        }
+    }
+</style>
